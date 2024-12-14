f67fdeca66f77327ac3e1c4829feaa96c375eb68
Kai...!! <achintya0071@gmail.com>
2024-12-14 18:54:14 +0530

    Refactor: Organize component exports in index.js

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+import GalleryPage from './GalleryPage';
+import GallerySection from './GallerySection';
+import HomeSection from './HomeSection';
+import Navbar from './Navbar';
+
+export { About, Contact, Footer, GalleryPage, GallerySection, HomeSection, Navbar };
